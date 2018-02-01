6d9b43ffcc7800466275bd833431f4c15c535480
Scott Saltzman <salty923@gmail.com>
2018-01-31 20:51:51 -0600

    initail key setup and structure

--- liri.js
+++ liri.js
@@ -1,6 +1,27 @@
+
+//adding .env file with api keys
 require("dotenv").config();
 
+//adding node file system package for read, write, append, etc
+var fs = require('fs');
+
+//adding js keys
 var keys = require("./keys.js");
 
+//adding twitter npm
+var Twitter = require('twitter');
+
+//adding spotify npm
+var Spotify = require('node-spotify-api');
+
+//adding request npm for http calls
+var request = require('request');
+
+//accessing keys 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var omdb = new omdb(keys.OMDB);
+
+//process arugments
+var service = process.argv[2];
+var search = process.argv[3];
